2208af81bcc7db8ed9f9987f81fba12a7f4a01ec
Eve Beckett <evebeckettwriter@gmail.com>
2021-09-24 17:06:36 -0400

    converting to hexadecimals

---
+++ 01-color-flipper/final/README.md
@@ -0,0 +1,4 @@
+WHAT I DID:
+
+The CSS and HTML were created as part of the tutorial, not by me.  I was responsible for all creating, connecting, and writing all code for both javascript files (app.js and hex.js)
+
--- 01-color-flipper/final/hex.js
+++ 01-color-flipper/final/hex.js
@@ -1,23 +1,18 @@
 const colorArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C","D", "E", "F"];
 
-const colors = getRandomColor();
 
 const button = document.getElementById("btn");
 const color = document.querySelector(".color");
-button.addEventListener("click", function(){
-    document.body.style.backgroundColor = getRandomColor();
-    color.textContent = document.body.style.backgroundColor;
-})
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * colorArray.length);
-}
-
-function getRandomColor() {
+button.addEventListener("click", function () {
     let hexColor = "#";
-    for (let count = 1; count <= 6; count++) {
+    for (let count = 0; count < 6; count++) {
         hexColor = hexColor + colorArray[getRandomNumber()];
     }
-    console.log(hexColor);
-    return hexColor;
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+})
+
+    
+function getRandomNumber() {
+    return Math.floor(Math.random() * colorArray.length);
 }
